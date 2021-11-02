db94d5d8979083269f3b6e33955c03058795989a
YunLong Zhou <yunlong.zhou@thoughtworks.com>
2021-11-02 10:28:04 +0800

    update: complete Queue

--- Javascript/Queue.ts
+++ Javascript/Queue.ts
@@ -14,15 +14,40 @@ class Queue {
   }
 
   shift() {
+    if (this.isEmpty()) return null
 
+    const r = this.items[0]
+
+    this.length--
+
+    for (let i = 0; i < this.length; i++) {
+      this.items[i] = this.items[i + 1]
+    }
+
+    delete this.items[this.length]
+
+    return r
   }
 
   insert(i, node) {
+    if (i > this.length - 1) this.push(node)
 
+    for (let j = this.length - 1; j > i; j++) {
+      this.items[j + 1] = this.items[j]
+    }
+
+    this.items[i] = node
+    this.length++
   }
 
   out(i) {
+    if (i > this.length - 1) return this.shift()
 
+    for (let j = i; j < this.length - 1; j++) {
+      this.items[j] = this.items[j + 1]
+    }
+
+    this.length--
   }
 
   isEmpty() {
