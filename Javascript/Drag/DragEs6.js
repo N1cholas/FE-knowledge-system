dd141e8b39c8a95281343475c1bf40148c271873
YunLong Zhou <yunlong.zhou@thoughtworks.com>
2021-11-14 22:50:59 +0800

    update: complete Drag according task list

--- Javascript/Drag/DragEs6.js
+++ Javascript/Drag/DragEs6.js
@@ -1,56 +1,59 @@
 var Drag = /** @class */ (function () {
-    function Drag(targetID) {
-        this.target = document.getElementById(targetID);
+    function Drag(target) {
+        this.target = document.getElementById(target);
         this.transform = this.getTransform();
         this.init();
     }
-    Drag.prototype.getTransform = function () {
-        var _a;
-        var divStyle = document.createElement('div').style;
-        var transformList = ['transform', 'webkitTransform', 'MozTransform', 'OTransform'];
-        var transformKey = (_a = transformList.filter(function (transform) { return transform in divStyle; })) === null || _a === void 0 ? void 0 : _a[0];
-        return transformKey ? transformKey : '';
-    };
     Drag.prototype.getStyle = function (property) {
-        var targetStyle = window.getComputedStyle(this.target);
-        return targetStyle[property];
+        return window.getComputedStyle(this.target)[property];
+    };
+    Drag.prototype.getTransform = function () {
+        var transformList = ['transform', 'webkitTransform', 'MozTransform', 'OTransform'];
+        var divStyle = document.createElement('div').style;
+        var result = transformList.filter(function (transform) { return transform in divStyle; });
+        return result[0];
     };
     Drag.prototype.getPosition = function () {
-        var transformValue = this.getStyle(this.transform);
-        if (transformValue === 'none') {
+        var transformMatrix = this.getStyle(this.transform);
+        if (transformMatrix === 'none') {
             return { x: 0, y: 0 };
         }
-        else {
-            var transformX = transformValue.match(/-?\d+/g)[4];
-            var transformY = transformValue.match(/-?\d+/g)[5];
-            return { x: Number(transformX), y: Number(transformY) };
-        }
+        return {
+            x: Number(transformMatrix.match(/-?\d+/g)[4]),
+            y: Number(transformMatrix.match(/-?\d+/g)[5])
+        };
     };
     Drag.prototype.setPosition = function (position) {
-        var targetStyle = this.target.style;
-        targetStyle[this.transform] = "translate(" + position.x + "px, " + position.y + "px)";
+        this.target.style[this.transform] = "translate(" + position.x + "px, " + position.y + "px)";
     };
     Drag.prototype.init = function () {
         var _this = this;
+        var dragging = false;
+        var dragEnd = function () {
+            document.removeEventListener('mousemove', dragMove);
+            document.removeEventListener('mouseup', dragEnd);
+            dragging = false;
+        };
+        var dragMove = function (e) {
+            if (!dragging)
+                return;
+            var dragDistanceX = e.pageX - _this.dragStartX;
+            var dragDistanceY = e.pageY - _this.dragStartY;
+            _this.setPosition({
+                x: _this.targetX + dragDistanceX,
+                y: _this.targetY + dragDistanceY
+            });
+        };
         var dragStart = function (e) {
-            _this.x = _this.getPosition().x;
-            _this.y = _this.getPosition().y;
-            _this.startDragX = e.pageX;
-            _this.startDragY = e.pageY;
-            var dragMove = function (e) {
-                _this.setPosition({
-                    x: e.pageX - _this.startDragX + _this.x,
-                    y: e.pageY - _this.startDragY + _this.y
-                });
-            };
-            var dragEnd = function () {
-                document.removeEventListener('mousemove', dragMove);
-                document.removeEventListener('mouseup', dragEnd);
-            };
-            document.addEventListener('mousemove', dragMove, false);
-            document.addEventListener('mouseup', dragEnd, false);
+            _this.dragStartX = e.pageX;
+            _this.dragStartY = e.pageY;
+            _this.targetX = _this.getPosition().x;
+            _this.targetY = _this.getPosition().y;
+            dragging = true;
         };
         this.target.addEventListener('mousedown', dragStart, false);
+        this.target.addEventListener('mousemove', dragMove, false);
+        this.target.addEventListener('mouseup', dragEnd, false);
     };
     return Drag;
 }());
--- Javascript/Drag/DragEs6.ts
+++ Javascript/Drag/DragEs6.ts
@@ -3,79 +3,83 @@ type Position = {
   y: number
 }
 
+
 class Drag {
   private readonly target: HTMLElement
-  private x: number
-  private y: number
-  private startDragX: number
-  private startDragY: number
   private readonly transform: string
+  private dragStartX: number
+  private dragStartY: number
+  private targetX: number
+  private targetY: number
 
-  constructor(targetID: string) {
-    this.target = document.getElementById(targetID)
+  constructor(target: string) {
+    this.target = document.getElementById(target)
     this.transform = this.getTransform()
     this.init()
   }
 
-  getTransform(): string {
-    const divStyle: CSSStyleDeclaration = document.createElement('div').style
-    const transformList: string[] = ['transform', 'webkitTransform', 'MozTransform', 'OTransform']
-
-    const transformKey: string = transformList.filter(transform => transform in divStyle)?.[0]
-
-    return transformKey ? transformKey : ''
+  getStyle(property: string): string {
+    return window.getComputedStyle(this.target)[property]
   }
 
-  getStyle(property: string): string {
-    const targetStyle = window.getComputedStyle(this.target)
+  getTransform(): string {
+    const transformList = ['transform', 'webkitTransform', 'MozTransform', 'OTransform']
 
-    return targetStyle[property]
+    const divStyle: CSSStyleDeclaration = document.createElement('div').style
+
+    const result = transformList.filter(transform => transform in divStyle)
+
+    return result[0]
   }
 
   getPosition(): Position {
-    const transformValue = this.getStyle(this.transform)
+    const transformMatrix = this.getStyle(this.transform)
 
-    if (transformValue === 'none') {
-      return {x: 0, y: 0}
-    } else {
-      const transformX = transformValue.match(/-?\d+/g)[4]
-      const transformY = transformValue.match(/-?\d+/g)[5]
+    if (transformMatrix === 'none') {
+      return { x: 0, y: 0 }
+    }
 
-      return {x: Number(transformX), y: Number(transformY)}
+    return {
+      x: Number(transformMatrix.match(/-?\d+/g)[4]),
+      y: Number(transformMatrix.match(/-?\d+/g)[5])
     }
   }
 
   setPosition(position: Position): void {
-    const targetStyle = this.target.style
-
-    targetStyle[this.transform] = `translate(${position.x}px, ${position.y}px)`
+    this.target.style[this.transform] = `translate(${position.x}px, ${position.y}px)`
   }
 
   init(): void {
-    const dragStart = (e: MouseEvent): void => {
-      this.x = this.getPosition().x
-      this.y = this.getPosition().y
-      this.startDragX = e.pageX
-      this.startDragY = e.pageY
+    let dragging = false
 
+    const dragEnd = () => {
+      document.removeEventListener('mousemove', dragMove)
+      document.removeEventListener('mouseup', dragEnd)
+      dragging = false
+    }
 
-      const dragMove = (e: MouseEvent) => {
-        this.setPosition({
-          x: e.pageX - this.startDragX + this.x,
-          y: e.pageY - this.startDragY + this.y
-        })
-      }
+    const dragMove = (e: MouseEvent) => {
+      if (!dragging) return
 
-      const dragEnd = () => {
-        document.removeEventListener('mousemove', dragMove)
-        document.removeEventListener('mouseup', dragEnd)
-      }
+      const dragDistanceX = e.pageX - this.dragStartX
+      const dragDistanceY = e.pageY - this.dragStartY
 
-      document.addEventListener('mousemove', dragMove, false)
-      document.addEventListener('mouseup', dragEnd, false)
+      this.setPosition({
+        x: this.targetX + dragDistanceX,
+        y: this.targetY + dragDistanceY,
+      })
+    }
+
+    const dragStart = (e: MouseEvent) => {
+      this.dragStartX = e.pageX
+      this.dragStartY = e.pageY
+      this.targetX = this.getPosition().x
+      this.targetY = this.getPosition().y
+      dragging = true
     }
 
     this.target.addEventListener('mousedown', dragStart, false)
-
+    this.target.addEventListener('mousemove', dragMove, false)
+    this.target.addEventListener('mouseup', dragEnd, false)
   }
 }
