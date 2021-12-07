0fc2659872e02018c613389ddd9e40d78fed9923
YunLong Zhou <yunlong.zhou@thoughtworks.com>
2021-12-07 16:39:50 +0800

    algorithm: min stack

--- Algorithm/MinStack.js
+++ Algorithm/MinStack.js
@@ -1,19 +1,21 @@
-
+/**
+ * initialize your data structure here.
+ */
 var MinStack = function() {
     this.s1 = []
-    this.s2 = [Number.MAX_SAFE_INTEGER]
+    this.s2 = [Infinity]
 };
 
 /**
- * @param {number} val
+ * @param {number} x
  * @return {void}
  */
-MinStack.prototype.push = function(val) {
-    this.s1.push(val)
+MinStack.prototype.push = function(x) {
+    this.s1.push(x)
     this.s2.push(
         Math.min(
-            val,
-            this.s2[this.s2.length - 1]
+            this.s2[this.s2.length - 1],
+            x
         )
     )
 };
@@ -36,15 +38,15 @@ MinStack.prototype.top = function() {
 /**
  * @return {number}
  */
-MinStack.prototype.getMin = function() {
+MinStack.prototype.min = function() {
     return this.s2[this.s2.length - 1]
 };
 
 /**
  * Your MinStack object will be instantiated and called as such:
  * var obj = new MinStack()
- * obj.push(val)
+ * obj.push(x)
  * obj.pop()
  * var param_3 = obj.top()
- * var param_4 = obj.getMin()
+ * var param_4 = obj.min()
  */
