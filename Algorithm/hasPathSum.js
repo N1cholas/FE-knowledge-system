2fb432617a5b75ac259bfb9528e61093249aef99
YunLong Zhou <yunlong.zhou@thoughtworks.com>
2021-12-08 18:13:52 +0800

    algorithm: has path sum

---
+++ Algorithm/hasPathSum.js
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} targetSum
+ * @return {boolean}
+ */
+var hasPathSum = function(root, targetSum) {
+    if (!root) return false
+    
+    if (!root.left && !root.right) return root.val === targetSum
+    
+    if (hasPathSum(root.left, targetSum - root.val)) {
+        return true
+    }
+    
+    if (hasPathSum(root.right, targetSum - root.val)) {
+        return true
+    }
+    
+    return false
+};
