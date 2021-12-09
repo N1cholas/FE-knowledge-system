4d5be38e07b2242dc8e503604225956cdff909e7
YunLong Zhou <yunlong.zhou@thoughtworks.com>
2021-12-09 14:08:32 +0800

    algorithm: sorted array to BST

---
+++ Algorithm/sortedArray2BST.js
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {number[]} nums
+ * @return {TreeNode}
+ */
+var sortedArrayToBST = function(nums) {
+    function toBST(array, l, r) {
+        if (l > r) return null
+        
+        const mid = Math.floor((r - l) / 2) + l
+        
+        const node = new TreeNode(nums[mid])
+        
+        node.left = toBST(nums, l, mid - 1)
+        node.right = toBST(nums, mid + 1, r)
+        
+        return node
+    }
+
+    return toBST(nums, 0, nums.length - 1)
+};
