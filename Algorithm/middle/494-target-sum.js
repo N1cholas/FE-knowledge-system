a52f81795a97c5d9b4e43c7c35a33c863a999533
YunLong Zhou <yunlong.zhou@thoughtworks.com>
2022-01-02 23:02:15 +0800

    algorithm: target sum

---
+++ Algorithm/middle/494-target-sum.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+
+// memory search
+var findTargetSumWays = function(nums, target) {
+    function trySum(nums, index, target) {
+        if (nums.length === index) {
+            return target === 0
+        }
+        
+        if (memo.has(`${index}-${target}`)) return memo.get(`${index}-${target}`)
+        
+        let res = 0
+        res += trySum(nums, index + 1, target + nums[index])
+        res += trySum(nums, index + 1, target - nums[index])
+        
+        memo.set(`${index}-${target}`, res)
+        
+        return res
+    }
+    
+    const memo = new Map()
+    
+    return trySum(nums, 0, target)
+};
