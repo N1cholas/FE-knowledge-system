2d3369287ea6920deccd3bf87e503223cb181021
YunLong Zhou <yunlong.zhou@thoughtworks.com>
2022-01-11 21:38:36 +0800

    algorithm: 3 way quick sort 👏🏻

--- Algorithm/sort/quick-sort.js
+++ Algorithm/sort/quick-sort.js
@@ -1,44 +1,47 @@
-function quickSort(array, l, r) {
-    function swap(array, l, r) {
-        [array[l], array[r]] = [array[r], array[l]]
-    }
-    function _partition(array, l, r) {
-        const randomIndex = Math.floor(Math.random() * (r - l + 1) + l)
-        
-        swap(array, l, randomIndex)
-        
-        const v = array[l]
-        
-        let i = l + 1
-        let j = r
-        
-        while (1) {
-            while (i <= r && array[i] < v) i++
-            while (j >= l + 1 && array[j] > v) j--
-            if (i > j) break
-            swap(array, i, j)
-            i++
-            j--
-        }
-        
-        swap(array, l, j)
-        
-        return j
-    }
-    
+const quickSort = (array, l, r) => {
     if (l >= r) {
         return
     }
     
-    const j = _partition(array, l, r)
-    quickSort(array, l, j - 1)
-    quickSort(array, j + 1, r)
+    function swap(array, l, r) {
+        const temp = array[l]
+        array[l] = array[r]
+        array[r] = temp
+    }
+    
+    const randomIndex = Math.floor(Math.random() * (r - l + 1) + l)
+    
+    swap(array, l, randomIndex)
+    
+    const v = array[l]
+    
+    let lt = l     // array[l + 1...lt] < v
+    let gt = r + 1 // array[gt...r] > v
+    let i = l + 1  // array[lt + 1...i) < v
+    
+    while(i < gt) {
+        if (array[i] < v) {
+            swap(array, i, lt + 1)
+            i++
+            lt++
+        } else if (array[i] > v) {
+            swap(array, i, gt - 1)
+            gt--
+        } else {
+            i++
+        }
+    }
+    
+    swap(array, l, lt)
+    
+    quickSort(array, l, lt - 1)
+    quickSort(array, gt, r)
 }
 
 const size = 1000000
 const array = new Array(size).fill(0).map(() => Math.floor(Math.random() * size))
 
-quickSort(array, 0, size)
+quickSort(array, 0, size - 1)
 
 let res = true
 
