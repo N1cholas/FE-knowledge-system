9b5a061c499706c12ff86d50e5f0ebacfbe61132
YunLong Zhou <yunlong.zhou@thoughtworks.com>
2022-01-26 15:01:34 +0800

    algorithm: third max 👏🏻

---
+++ Algorithm/easy/414-third-max.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var thirdMax = function(nums) {
+    let a = -Infinity
+    let b = -Infinity
+    let c = -Infinity
+    
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] > a) {
+            c = b
+            b = a
+            a = nums[i]
+        } else if (a > nums[i] && nums[i] > b) {
+            c = b
+            b = nums[i]
+        } else if (b > nums[i] && nums[i] > c) {
+            c = nums[i]
+        }
+    }
+    
+    return c === -Infinity ? a : c
+};
