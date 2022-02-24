d6064e6138abd5d9689767a0a3ce825246804b79
YunLong Zhou <yunlong.zhou@thoughtworks.com>
2022-02-24 14:14:22 +0800

    algorithm: split balance string 👏🏻

--- Algorithm/easy/1221-split-balance-string.js
+++ Algorithm/easy/1221-split-balance-string.js
@@ -2,18 +2,21 @@
  * @param {string} s
  * @return {number}
  */
+
+// greed
+// 贪心：尽可能分割最小的平衡字符串
 var balancedStringSplit = function(s) {
-    let counts = 0
     let ans = 0
+    let counts = 0
     
-    for (let i = 0; i < s.length; i++) {
+    for(let i = 0, len = s.length; i < len; i++) {
         if (s.charAt(i) === 'L') {
             counts++
         } else {
             counts--
         }
         
-        if (counts === 0) ans++
+        !counts && (ans++)
     }
     
     return ans
