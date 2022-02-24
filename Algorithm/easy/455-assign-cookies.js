e5375c0a74ab8fc24acea0b652f881b33a7c0359
YunLong Zhou <yunlong.zhou@thoughtworks.com>
2022-02-24 11:12:10 +0800

    algorithm: assign cookies 👏🏻

--- Algorithm/easy/455-assign-cookies.js
+++ Algorithm/easy/455-assign-cookies.js
@@ -3,23 +3,22 @@
  * @param {number[]} s
  * @return {number}
  */
+
+// greed
+// 贪心的思想：把最大的饼干分给胃口最大的小朋友，那么后面的最大的饼干也是分给后面胃口最大的小朋友
 var findContentChildren = function(g, s) {
     g.sort((a, b) => b - a)
     s.sort((a, b) => b - a)
     
     let gi = 0
     let si = 0
-    let counts = 0
     
-    while (gi < g.length && si < s.length) {
+    while (gi < g.length) {
         if (s[si] >= g[gi]) {
-            counts++
             si++
-            gi++
-        } else {
-            gi++
         }
+        gi++
     }
     
-    return counts
+    return si
 };
