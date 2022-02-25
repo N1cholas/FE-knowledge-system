ccac550a55a04ba5426d7c679f2175ae3cc32cfc
YunLong Zhou <yunlong.zhou@thoughtworks.com>
2022-02-25 16:15:29 +0800

    algorithm: solution for decode way 👏🏻

--- Algorithm/middle/091-decode-way.js
+++ Algorithm/middle/091-decode-way.js
@@ -3,25 +3,32 @@
  * @return {number}
  */
 
-// memory search
+// dynamic program
+// 状态转移方程:
+// when s[n] != '0'
+    // f(n)=f(n)+f(n-1)
+// when s[n - 1] != '0' & s[n - 1] * 10 + s[n] <= 26
+    // f(n)=f(n)+f(n-2)
 var numDecodings = function(s) {
-    function dfs(s, start) {
-        if (start >= s.length) return 1
-        if (s[start] === '0') return 0
-        
-        if (memo[start] !== -1) return memo[start]
-        
-        let res = dfs(s, start + 1)
-        if (start + 1 < s.length && Number(s.slice(start, start + 2)) <= 26) {
-            res += dfs(s, start + 2)
+    const dp = new Array(s.length + 1).fill(0)
+    dp[0] = 1
+    
+    for (let i = 1; i <= s.length; i++) {
+        // 当前解码使用1个字符
+        // 所以[0, i]的解码数等于f(i) + f(i-1)
+        if (s.charAt(i - 1) !== '0') {
+            dp[i] += dp[i - 1]
         }
-        
-        memo[start] = res
-        
-        return res
+        // 当前解码使用两个字符
+        // 所以[0, i]的解码数等于f(i) + f(i-2)
+        if (
+            i > 1
+            && s.charAt(i - 2) !== '0'
+            && (10 * Number(s.charAt(i - 2)) + Number(s.charAt(i - 1))) <= 26
+        ) {
+            dp[i] += dp[i - 2]
+        }
     }
     
-    const memo = new Array(s.length).fill(-1)
-    
-    return dfs(s, 0)
+    return dp[s.length]
 };
