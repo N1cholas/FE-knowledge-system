64b69237d1b1d87fa234748e18fd5aa684a81cc9
YunLong Zhou <yunlong.zhou@thoughtworks.com>
2022-02-25 13:34:52 +0800

    algorithm: 2 solutions for climb stairs 👏🏻

--- Algorithm/easy/070-climb-stairs.js
+++ Algorithm/easy/070-climb-stairs.js
@@ -2,15 +2,38 @@
  * @param {number} n
  * @return {number}
  */
+
+// memory search
 var climbStairs = function(n) {
-    const memo = new Array(n + 1).fill(-1)
-    
-    memo[0] = 1
-    memo[1] = 1
-    
-    for (let i = 2; i <= n; i++) {
-        memo[i] = memo[i - 1] + memo[i - 2]
+    function tryClimb(n) {
+        if (n === 1 || n === 0) return 1
+        
+        if (memo[n] !== -1) return memo[n]
+        
+        let res = tryClimb(n - 1) + tryClimb(n - 2)
+        
+        memo[n] = res
+        
+        return res
     }
     
-    return memo[n]
+    const memo = new Array(n + 1).fill(-1)
+    
+    return tryClimb(n)
+};
+
+// dynamic program
+// f(n) = f(n - 1) + f(n - 2)
+// f(1) = 1
+// f(0) = 1
+var climbStairs2 = function(n) {
+    const dp = new Array(n + 1)
+    dp[0] = 1
+    dp[1] = 1
+    
+    for (let i = 2; i <= n; i++) {
+        dp[i] = dp[i - 1] + dp[i - 2]
+    }
+    
+    return dp[n]
 };
