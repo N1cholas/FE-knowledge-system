9de354c801aea8d0dba6f4575464bb37346f64f7
YunLong Zhou <yunlong.zhou@thoughtworks.com>
2022-02-25 12:19:26 +0800

    algorithm: solution for unique paths II 👏🏻

--- Algorithm/middle/063-unique-paths-II.js
+++ Algorithm/middle/063-unique-paths-II.js
@@ -2,39 +2,45 @@
  * @param {number[][]} obstacleGrid
  * @return {number}
  */
+
+// dynamic program
+// 与unique paths I不同的是有障碍物
+// 初始化边界需要考虑障碍物，计算路径和跳过障碍物格子计算
 var uniquePathsWithObstacles = function(obstacleGrid) {
-    if (obstacleGrid[0][0]) return 0
-    const dp = []
     const m = obstacleGrid.length
     const n = obstacleGrid[0].length
     
-    for (let i = 0; i < m; i++) {
-        const cur = []
-        for (let j = 0; j < n; j++) {
-            cur.push(0)
+    if (obstacleGrid[0][0]) return 0
+    
+    const dp = []
+    
+    for(let i = 0; i < m; i++) {
+        const row = []
+        for(let j = 0; j < n; j++) {
+            row.push(0)
         }
-        dp.push(cur)
+        dp.push(row)
     }
     
     dp[0][0] = 1
     
-    for (let i = 1; i < n; i++) {
-        if (!obstacleGrid[0][i]) {
-            dp[0][i] = dp[0][i - 1]
-        }
-    }
-    
-    for (let i = 1; i < m; i++) {
-        if (!obstacleGrid[i][0]) {
+    for(let i = 1; i < m; i++) {
+        if(!obstacleGrid[i][0]) {
             dp[i][0] = dp[i - 1][0]
         }
     }
     
-    for (let i = 1; i < m; i++) {
-        for (let j = 1; j < n; j++) {
-            if (obstacleGrid[i][j]) continue
-            
-            dp[i][j] = dp[i][j - 1] + dp[i - 1][j]
+    for(let i = 1; i < n; i++) {
+        if(!obstacleGrid[0][i]) {
+            dp[0][i] = dp[0][i - 1]
+        }
+    }
+    
+    for(let i = 1; i < m; i++) {
+        for(let j = 1; j < n; j++) {
+            if (!obstacleGrid[i][j]) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+            }
         }
     }
     
