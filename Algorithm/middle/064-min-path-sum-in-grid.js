20aa81aee4f269bc1d24ffd1e7d437117a565194
YunLong Zhou <yunlong.zhou@thoughtworks.com>
2022-02-25 13:26:52 +0800

    algorithm: solution for min path sum in grid 👏🏻

--- Algorithm/middle/064-min-path-sum-in-grid.js
+++ Algorithm/middle/064-min-path-sum-in-grid.js
@@ -2,20 +2,24 @@
  * @param {number[][]} grid
  * @return {number}
  */
+
+// dynamic program
+// f(m,n) = f(m,n) + min{f(m-1, n), f(m,n-1)}
+// f(m,n)代表当前[m,n]的最小路径和
 var minPathSum = function(grid) {
     const m = grid.length
     const n = grid[0].length
     
-    for (let i = 1; i < n; i++) {
+    for(let i = 1; i < m; i++) {
+        grid[i][0] += grid[i - 1][0]
+    }
+    
+    for(let i = 1; i < n; i++) {
         grid[0][i] += grid[0][i - 1]
     }
     
-    for (let j = 1; j < m; j++) {
-        grid[j][0] += grid[j - 1][0]
-    }
-    
-    for (let i = 1; i < m; i++) {
-        for (let j = 1; j < n; j++) {
+    for(let i = 1; i < m; i++) {
+        for(let j = 1; j < n; j++) {
             grid[i][j] += Math.min(grid[i - 1][j], grid[i][j - 1])
         }
     }
