39ac68d8a19b7596ef4a6e9b7a6b105b60b50fcd
YunLong Zhou <yunlong.zhou@thoughtworks.com>
2022-02-27 11:53:01 +0800

    algorithm: best time to buy stock 👏🏻

--- algorithm/middle/309-best-time-to-buy-stock.js
+++ algorithm/middle/309-best-time-to-buy-stock.js
@@ -4,25 +4,21 @@
  */
 var maxProfit = function(prices) {
     const dp = []
+    const n = prices.length
     
-    for (let i = 0; i < prices.length; i++) {
+    for(let i = 0; i < n; i++) {
         dp.push(new Array(3).fill(0))
     }
     
-    // 0 持有
-    // 1 不持有并且当天没有卖出
-    // 2 不持有并且当天卖出
-    dp[0][0] = -prices[0]
-    dp[0][1] = 0
-    dp[0][2] = 0
+    dp[0][0] = -prices[0] // 持有股票
+    dp[0][1] = 0          // 不持有股票且没有卖出
+    dp[0][2] = 0          // 不持有股票且卖出
     
-    for (let i = 1; i < prices.length; i++) {
+    for(let i = 1; i < n; i++) {
         dp[i][0] = Math.max(dp[i - 1][0], -prices[i] + dp[i - 1][1])
-        dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][2])
-        dp[i][2] = dp[i - 1][0] + prices[i]
+        dp[i][1] = Math.max(dp[i - 1][2], dp[i - 1][1])
+        dp[i][2] = prices[i] + dp[i - 1][0]
     }
     
-    const lastIndex = prices.length - 1
-    
-    return Math.max(dp[lastIndex][1], dp[lastIndex][2])
+    return Math.max(dp[n - 1][1], dp[n - 1][2])
 };
