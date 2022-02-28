9aab70844d851629e6458f2952341cbfca18e7a9
YunLong Zhou <yunlong.zhou@thoughtworks.com>
2022-02-28 11:32:11 +0800

    algorithm: partition equal subset sum 👏🏻

--- algorithm/README.md
+++ algorithm/README.md
@@ -183,6 +183,8 @@
 > 使用动态规划需要解决两个问题
 > 1. 找到重复的子问题
 > 2. 存储子问题的答案
+> 3. 确定状态方程（推出状态转移方程）
+> 4. 确定边界
 - [053-max-sub-array](easy/053-max-sub-array.js)
 - [062-unique-paths](middle/062-unique-paths.js)
 - [063-unique-paths-II](middle/063-unique-paths-II.js)
--- algorithm/middle/416-partition-equal-subset-sum.js
+++ algorithm/middle/416-partition-equal-subset-sum.js
@@ -31,3 +31,33 @@ var canPartition = function(nums) {
     
     return partition(nums, nums.length - 1, sum / 2)
 };
+
+// dp[i][j]表示在[0, i]之间的nums能找出几个数的和为j
+var canPartition2 = function(nums) {
+    const n = nums.length;
+    const sum = nums.reduce((p, c) => p + c)
+    
+    if (sum % 2) return false
+    
+    const target = sum / 2
+    
+    const dp = new Array(n).fill(0).map(_ => new Array(target + 1).fill(false));
+    
+    for (let i = 0; i < n; i++) {
+        dp[i][0] = true;
+    }
+    
+    dp[0][nums[0]] = true;
+    
+    for (let i = 1; i < n; i++) {
+        const num = nums[i];
+        for (let j = 1; j <= target; j++) {
+            if (j >= num) {
+                dp[i][j] = dp[i - 1][j] || dp[i - 1][j - num];
+            } else {
+                dp[i][j] = dp[i - 1][j];
+            }
+        }
+    }
+    return dp[n - 1][target];
+};
