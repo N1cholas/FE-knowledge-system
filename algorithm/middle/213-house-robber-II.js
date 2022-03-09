7c4f6e4aa6e347c78e80fed58f78399eceb7aa02
YunLong Zhou <yunlong.zhou@thoughtworks.com>
2022-03-09 23:04:45 +0800

    algorithm: solution of house robber II 👏🏻

--- algorithm/middle/213-house-robber-II.js
+++ algorithm/middle/213-house-robber-II.js
@@ -2,24 +2,26 @@
  * @param {number[]} nums
  * @return {number}
  */
-
-// 算两次
 var rob = function(nums) {
-    if (nums.length === 1) return nums[0]
+    if (nums.length < 2) return nums[0]
+    const n1 = nums.slice(1)
+    const n2 = nums.slice(0, nums.length - 1)
     
-    function tryRobber(nums) {
+    function tryRob(nums) {
         const n = nums.length
-        const dp = new Array(n).fill(-1)
+        
+        const dp = new Array(n).fill(0)
         
         dp[0] = nums[0]
+        
         dp[1] = Math.max(nums[1], dp[0])
         
-        for (let i = 2; i < n; i++) {
+        for(let i = 2; i < n; i++) {
             dp[i] = Math.max(nums[i] + dp[i - 2], dp[i - 1])
         }
         
         return dp[n - 1]
     }
     
-    return Math.max(tryRobber(nums.slice(0, nums.length - 1)), tryRobber(nums.slice(1)))
+    return Math.max(tryRob(n1), tryRob(n2))
 };
