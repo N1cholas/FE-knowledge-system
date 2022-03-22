7c90da7971ea8c80c714d429827e9f47ca682ac0
YunLong Zhou <yunlong.zhou@thoughtworks.com>
2022-03-22 11:26:06 +0800

    algorithm: solution of partition equal subset sum 👏🏻

--- algorithm/middle/416-partition-equal-subset-sum.js
+++ algorithm/middle/416-partition-equal-subset-sum.js
@@ -32,32 +32,35 @@ var canPartition = function(nums) {
     return partition(nums, nums.length - 1, sum / 2)
 };
 
-// dp[i][j]表示在[0, i]之间的nums能找出几个数的和为j
+// dynamic program
+// 定义dp: dp[i][j] 表示前i个nums数中能否找到和为j
+// 状态方程:
+//               不选择nums[i]     选择nums[i]
+//     f(i, j) = f(i - 1, j) || f(i - 1, j - nums[i]) & j >= nums[i]
+//     f(i, j) = f(i - 1, j) & j < nums[i]
+// 边界：f(0, 0) = true
 var canPartition2 = function(nums) {
-    const n = nums.length;
     const sum = nums.reduce((p, c) => p + c)
     
     if (sum % 2) return false
     
     const target = sum / 2
     
-    const dp = new Array(n).fill(0).map(_ => new Array(target + 1).fill(false));
+    const n = nums.length
     
-    for (let i = 0; i < n; i++) {
-        dp[i][0] = true;
-    }
+    const dp = new Array(n + 1).fill(0).map(_ => new Array(target + 1).fill(false))
     
-    dp[0][nums[0]] = true;
+    dp[0][0] = true
     
-    for (let i = 1; i < n; i++) {
-        const num = nums[i];
-        for (let j = 1; j <= target; j++) {
-            if (j >= num) {
-                dp[i][j] = dp[i - 1][j] || dp[i - 1][j - num];
+    for(let i = 1; i <= n; i++) {
+        for(let j = 1; j <= target; j++) {
+            if (j >= nums[i - 1]) {
+                dp[i][j] = dp[i - 1][j] || dp[i - 1][j - nums[i - 1]]
             } else {
-                dp[i][j] = dp[i - 1][j];
+                dp[i][j] = dp[i - 1][j]
             }
         }
     }
-    return dp[n - 1][target];
+    
+    return dp[n][target]
 };
