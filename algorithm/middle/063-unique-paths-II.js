d7d2c7c814cf5ff51ebac5f6cac8eb415ac8da77
YunLong Zhou <yunlong.zhou@thoughtworks.com>
2022-03-24 10:58:08 +0800

    algorithm: solution of unique paths II 👏🏻

--- algorithm/middle/063-unique-paths-II.js
+++ algorithm/middle/063-unique-paths-II.js
@@ -3,18 +3,20 @@
  * @return {number}
  */
 
+// source https://leetcode-cn.com/problems/unique-paths-ii/
+
 // dynamic program
-// 与unique paths I不同的是有障碍物
-// 初始化边界需要考虑障碍物，计算路径和跳过障碍物格子计算
-// 障碍物格子的路径和为0
-// dp[i][j]表示在 i * j的矩阵内，第i - 1行第j - 1个格子的路径和
+// 定义dp: dp[i][j] 表示到达(i, j)这个格子的路径总和
+// 状态转移: f(i, j) = f(i - 1, j) + f(i, j - 1)
+// 初始化: dp[i][j] = 0
+// 边界: dp[0][0] = 1 & dp[i][0] = dp[i - 1][0] & dp[0][j] = dp[0][j - 1]
 var uniquePathsWithObstacles = function(obstacleGrid) {
-    if (obstacleGrid[0][0]) return 0
-    
     const m = obstacleGrid.length
     const n = obstacleGrid[0].length
     
     const dp = new Array(m).fill(0).map(_ => new Array(n).fill(0))
+    
+    if (obstacleGrid[0][0]) return 0
     
     dp[0][0] = 1
     
@@ -24,9 +26,9 @@ var uniquePathsWithObstacles = function(obstacleGrid) {
         }
     }
     
-    for(let i = 1; i < n; i++) {
-        if (!obstacleGrid[0][i]) {
-            dp[0][i] = dp[0][i - 1]
+    for(let j = 1; j < n; j++) {
+        if (!obstacleGrid[0][j]) {
+            dp[0][j] = dp[0][j - 1]
         }
     }
     
