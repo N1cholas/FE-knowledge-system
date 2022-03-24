aa91ecd1db9809a52821c7926b883538cc90a859
YunLong Zhou <yunlong.zhou@thoughtworks.com>
2022-03-24 12:02:04 +0800

    algorithm: solution of decode way 👏🏻

--- algorithm/middle/091-decode-way.js
+++ algorithm/middle/091-decode-way.js
@@ -3,33 +3,35 @@
  * @return {number}
  */
 
+// source https://leetcode-cn.com/problems/decode-ways/
+
 // dynamic program
-// dp[i]表示前i个字符解码总数，边界为0个字符的解码数是1
-// 状态转移方程:
-// when s[n] != '0'
-    // f(n)=f(n)+f(n-1)
-// when s[n - 1] != '0' & s[n - 1] * 10 + s[n] <= 26
-    // f(n)=f(n)+f(n-2)
+// 定义dp: dp[i]表示前i个字符解码的方案数
+// 状态转移:
+//   f(i) = f(i) + f(i - 1)
+//   f(i) = f(i) + f(i - 2)
+// 初始化: dp[i] = 0
+// 边界: dp[0] = 1
 var numDecodings = function(s) {
-        const n = s.length
-        
-        const dp = new Array(n + 1).fill(0)
-        
-        dp[0] = 1
-        
-        for(let i = 1; i <= n; i++) {
-            if (Number(s.charAt(i - 1)) !== 0) {
-                dp[i] += dp[i - 1]
-            }
-            
-            if (
-                i > 1
-                && Number(s.charAt(i - 2)) !== 0
-                && Number(s.substr(i - 2, 2)) <= 26
-            ) {
-                dp[i] += dp[i - 2]
-            }
+    const n = s.length
+    
+    const dp = new Array(n + 1).fill(0)
+    
+    dp[0] = 1
+    
+    for(let i = 1; i <= n; i++) {
+        if (s.charAt(i - 1) !== '0') {
+            dp[i] += dp[i - 1]
         }
         
-        return dp[n]
-    };
+        if (
+            i > 1
+            && s.charAt(i - 2) !== '0'
+            && Number(s.substr(i - 2, 2)) <= 26
+        ) {
+            dp[i] += dp[i - 2]
+        }
+    }
+    
+    return dp[n]
+};
