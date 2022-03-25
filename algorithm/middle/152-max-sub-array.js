b9f3a4e5e46d3b21a7341801f4a2740f2fb9974f
YunLong Zhou <yunlong.zhou@thoughtworks.com>
2022-03-25 12:12:33 +0800

    algorithm: solution of max sub array 👏🏻

--- algorithm/middle/152-max-sub-array.js
+++ algorithm/middle/152-max-sub-array.js
@@ -3,21 +3,36 @@
  * @return {number}
  */
 
+// source https://leetcode-cn.com/problems/maximum-product-subarray/
+
+// 定义dp:
+//   max[i] 表示[0, i]区间最大乘积
+//   min[i] 表示[0, i]区间最小乘积
+// 状态转移:
+//   fmax(i) = max{ fmax(i - 1) * nums[i], fmin(i - 1) * nums[i], nums[i] }
+//   fmin(i) = min{ fmin(i - 1) * nums[i], fmax(i - 1) * nums[i], nums[i] }
+// 初始化:
+//   max[i] = 1
+//   min[i] = 1
+// 边界:
+//   max[0] = nums[0]
+//   min[0] = nums[0]
 var maxProduct = function(nums) {
-    let ans = -Infinity
-    let max = 1
-    let min = 1
+    const n = nums.length
     
-    for (let i = 0; i < nums.length; i++) {
-        if (nums[i] < 0) {
-            [max, min] = [min, max]
-        }
-        
-        max = Math.max(nums[i], nums[i] * max)
-        min = Math.min(nums[i], nums[i] * min)
-        
-        ans = Math.max(ans, max)
+    const max = new Array(n).fill(1)
+    
+    max[0] = nums[0]
+    
+    const min = new Array(n).fill(1)
+    
+    min[0] = nums[0]
+    
+    for(let i = 1; i < n; i++) {
+        const num = nums[i]
+        max[i] = Math.max(max[i - 1] * num, min[i - 1] * num, num)
+        min[i] = Math.min(max[i - 1] * num, min[i - 1] * num, num)
     }
     
-    return ans
+    return Math.max(...max)
 };
