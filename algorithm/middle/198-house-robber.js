3b4e5c509b5d5f15bcd607349ce29797819991df
YunLong Zhou <yunlong.zhou@thoughtworks.com>
2022-03-27 10:11:58 +0800

    algorithm: solution of house robber 👏🏻

--- algorithm/middle/198-house-robber.js
+++ algorithm/middle/198-house-robber.js
@@ -3,8 +3,27 @@
  * @return {number}
  */
 
-// memory search
+// dynamic program
+// 定义dp: dp[i] 表示偷前i间房子的最高金额
+// 状态转移: f(i) = max{ nums[i - 1] + f(i - 2), f(i - 1) }
+// 初始化: dp[i] = 0
+// 边界: dp[1] = nums[0]
 var rob = function(nums) {
+    const n = nums.length
+    
+    const dp = new Array(n + 1).fill(0)
+    
+    dp[1] = nums[0]
+    
+    for(let i = 2; i <= n; i++) {
+        dp[i] = Math.max(nums[i - 1] + dp[i - 2], dp[i - 1])
+    }
+    
+    return dp[n]
+};
+
+// memory search
+var rob2 = function(nums) {
     function tryRob(nums, index) {
         if (index >= nums.length) {
             return 0
@@ -25,22 +44,4 @@ var rob = function(nums) {
     const memo = new Array(nums.length).fill(-1)
     
     return tryRob(nums, 0)
-};
-
-// dynamic program
-// f(i)=max{nums[i] + f(i - 2), f(i - 1)}
-// dp[i]表示[0, i - 1]的最大偷取值
-var rob2 = function(nums) {
-    const n = nums.length
-    
-    const dp = new Array(n).fill(0)
-    
-    dp[0] = nums[0]
-    dp[1] = Math.max(nums[1], dp[0])
-    
-    for(let i = 2; i < n; i++) {
-        dp[i] = Math.max(nums[i] + dp[i - 2], dp[i - 1])
-    }
-    
-    return dp[n - 1]
 };
