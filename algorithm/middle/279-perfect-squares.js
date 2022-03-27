e1184f1995585835c99390408b462153f09fcf9f
YunLong Zhou <yunlong.zhou@thoughtworks.com>
2022-03-27 10:36:28 +0800

    algorithm: solution of perfect squares 👏🏻

--- algorithm/middle/279-perfect-squares.js
+++ algorithm/middle/279-perfect-squares.js
@@ -3,8 +3,34 @@
  * @return {number}
  */
 
-// memory search
+// dynamic program
+// 定义dp: dp[i]表示整数i的最小完全平方数数量
+// 状态转移:
+//   f(i) = min{ f(i), 1 + f(i - j * j) }
+//   & j in [1, 2, 3, ... , i]
+//   & (i - j * j) >= 0
+// 初始化: dp[i] = Infinity
+// 边界: dp[0] = 0
 var numSquares = function(n) {
+    const dp = new Array(n + 1).fill(Infinity)
+    
+    dp[0] = 0
+    
+    for(let i = 1; i <= n; i++) {
+        for (let j = 1; j <= i; j++) {
+            if (i - j * j >= 0) {
+                dp[i] = Math.min(dp[i], 1 + dp[i - j * j])
+            } else {
+                break
+            }
+        }
+    }
+    
+    return dp[n]
+};
+
+// memory search
+var numSquares2 = function(n) {
     function trySquares(n) {
         if (n <= 0) {
             return 0
@@ -26,22 +52,4 @@ var numSquares = function(n) {
     const memo = new Array(n + 1).fill(Infinity)
     
     return trySquares(n)
-};
-
-// dynamic program
-// f(i)=min{f(i), 1 + f(i - j * j)} j => [1, 4, 9, 16...j * j]
-// f(i)表示当前n的最少的完全平方数
-// 0其实是完全平方数，但是这道题0不作为完全平方数
-var numSquares2 = function(n) {
-    const dp = new Array(n + 1).fill(Infinity)
-    
-    dp[0] = 0
-    
-    for(let i = 1; i <= n; i++) {
-        for(let j = 1; i - j * j >= 0; j++) {
-            dp[i] = Math.min(dp[i], 1 + dp[i - j * j])
-        }
-    }
-    
-    return dp[n]
 };
