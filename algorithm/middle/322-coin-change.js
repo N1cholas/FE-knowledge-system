192e4f015de58654479ddd5f5ddb5f120827155e
YunLong Zhou <yunlong.zhou@thoughtworks.com>
2022-03-28 23:20:04 +0800

    algorithm: solution of coin change 👏🏻

--- algorithm/middle/322-coin-change.js
+++ algorithm/middle/322-coin-change.js
@@ -4,8 +4,33 @@
  * @return {number}
  */
 
-// memory search
+// source https://leetcode-cn.com/problems/coin-change/
+
+// dynamic program
+// 定义dp: dp[i] 表示组成金额i最少的硬币个数
+// 状态转移:
+//   f(i) = min{ f(i), 1 + f(i - coins[j]) }
+//   & i > coins[j]
+// 初始化: dp[i] = Infinity
+// 边界: dp[0] = 0
 var coinChange = function(coins, amount) {
+    const dp = new Array(amount + 1).fill(Infinity)
+    
+    dp[0] = 0
+    
+    for(let i = 1; i <= amount; i++) {
+        for(const coin of coins) {
+            if (i - coin >= 0) {
+                dp[i] = Math.min( dp[i], 1 + dp[i - coin] )
+            }
+        }
+    }
+    
+    return dp[amount] === Infinity ? -1 : dp[amount]
+};
+
+// memory search
+var coinChange2 = function(coins, amount) {
     function tryChange(coins, amount) {
         if (amount === 0) {
             return 0
@@ -32,22 +57,4 @@ var coinChange = function(coins, amount) {
     const ans = tryChange(coins, amount, 0)
     
     return ans === max ? -1 : ans
-};
-
-// dynamic program
-// 定义dp: dp[i] 表示凑成金额i最少硬币个数
-// 确定状态方程: f(i) = min{ f(i), 1 + f(i - coin) } && i - coin >= 0 && coin in coins
-// 确定边界: f(0) 凑成金额0的最少硬币个数为0
-var coinChange2 = function(coins, amount) {
-    const dp = new Array(amount + 1).fill(Infinity)
-    
-    dp[0] = 0
-    
-    for(let i = 1; i <= amount; i++) {
-        for(const coin of coins) {
-            if (i - coin >= 0) dp[i] = Math.min(dp[i], 1 + dp[i - coin])
-        }
-    }
-    
-    return dp[amount] === Infinity ? -1 : dp[amount]
 };
