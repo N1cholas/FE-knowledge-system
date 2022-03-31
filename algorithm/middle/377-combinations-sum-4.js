f92af99c051ee4b2cc529b50459a3bf1199bfb26
YunLong Zhou <yunlong.zhou@thoughtworks.com>
2022-03-31 19:43:42 +0800

    algorithm: solution of combinations sum IV 👏🏻

--- algorithm/middle/377-combinations-sum-4.js
+++ algorithm/middle/377-combinations-sum-4.js
@@ -4,6 +4,32 @@
  * @return {number}
  */
 
+// source https://leetcode-cn.com/problems/combination-sum-iv/
+
+// dynamic program
+// 定义dp: dp[i] 表示组合数字i的个数
+// 状态转移:
+//   f(i) = f(i) + 1 + f(i - nums[j])
+//   & 0 < j < nums.length
+//   & i - num[j] >= 0
+// 初始化: dp[i] = 0
+// 边界: dp[0] = 1
+var combinationSum4_2 = function(nums, target) {
+    const dp = new Array(target + 1).fill(0)
+    
+    dp[0] = 1
+    
+    for(let i = 1; i <= target; i++) {
+        for(const num of nums) {
+            if (i - num >= 0) {
+                dp[i] += dp[i - num]
+            }
+        }
+    }
+    
+    return dp[target]
+};
+
 // memory search
 var combinationSum4 = function(nums, target) {
     function tryCombinationSum(nums, target) {
@@ -28,24 +54,4 @@ var combinationSum4 = function(nums, target) {
     const memo = new Array(target + 1).fill(-1)
     
     return tryCombinationSum(nums, target)
-};
-
-// dynamic program
-// dp[i] 表示组合数字i最大数目
-// 确定状态方程: f(i) = f(i) + f(i - num[i]) && i >= num[i]
-// 确定边界: f(0) = 1
-var combinationSum4_2 = function(nums, target) {
-    const dp = new Array(target + 1).fill(0)
-    
-    dp[0] = 1
-    
-    for(let i = 1; i <= target; i++) {
-        for(const num of nums) {
-            if (i >= num) {
-                dp[i] += dp[i - num]
-            }
-        }
-    }
-    
-    return dp[target]
 };
