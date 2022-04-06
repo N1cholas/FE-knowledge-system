1faf987ca66631286e8fb7be65069582f3add4a3
YunLong Zhou <yunlong.zhou@thoughtworks.com>
2022-04-06 13:22:04 +0800

    algorithm: solution of max uncross line 👏🏻

--- algorithm/middle/1035-max-uncross-line.js
+++ algorithm/middle/1035-max-uncross-line.js
@@ -4,10 +4,36 @@
  * @return {number}
  */
 
-// f(n1, n2, m, n) 获取n1上[0...m]和n2上[0...n] 最多不相交的线数量
+// source: https://leetcode-cn.com/problems/uncrossed-lines/
+
+// dynamic program
+// 定义dp: dp[i][j] 表示前i个数和前j个数的最大连线数
+// 状态转移:
+//   nums1[i] === nums2[j] & f(i, j) = 1 + f(i - 1, j - 1)
+//   nums1[i] !== nums2[j] & f(i, j) = max{ f(i - 1, j), f(i, j - 1) }
+// 初始化: dp[i][j] = 0
+// 边界: dp[0][0] = 0
+var maxUncrossedLines = function(nums1, nums2) {
+    const n1 = nums1.length
+    const n2 = nums2.length
+    
+    const dp = new Array(n1 + 1).fill(0).map(_ => new Array(n2 + 1).fill(0))
+    
+    for(let i = 1; i <= n1; i++) {
+        for(let j = 1; j <= n2; j++) {
+            if (nums1[i - 1] === nums2[j - 1]) {
+                dp[i][j] = 1 + dp[i - 1][j - 1]
+            } else {
+                dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1])
+            }
+        }
+    }
+    
+    return dp[n1][n2]
+};
 
 // memory search
-var maxUncrossedLines = function(nums1, nums2) {
+var maxUncrossedLines2 = function(nums1, nums2) {
     function tryGetMax(n1, n2, m, n) {
         if (m < 0 || n < 0) {
             return 0
@@ -33,29 +59,4 @@ var maxUncrossedLines = function(nums1, nums2) {
     const memo = new Map()
     
     return tryGetMax(nums1, nums2, nums1.length - 1, nums2.length - 1)
-};
-
-// dynamic program
-// 定义dp: dp[i][j] 表示前i个nums1和前j个nums2的最长公共子序列
-// 状态转移:
-// f(i, j) = 1 + f(i - 1, j - 1) & nums[i - 1] === nums[j - 1]
-// f(i, j) = max{ f(i - 1, j), f(i, j - 1) } & nums[i - 1] !== nums[j - 1]
-// 边界: dp[0][0] = 0
-var maxUncrossedLines2 = function(nums1, nums2) {
-    const n = nums1.length
-    const m = nums2.length
-    
-    const dp = new Array(n + 1).fill(0).map(_ => new Array(m + 1).fill(0))
-    
-    for(let i = 1; i <= n; i++) {
-        for(let j = 1; j <= m; j++) {
-            if (nums1[i - 1] === nums2[j - 1]) {
-                dp[i][j] = 1 + dp[i - 1][j - 1]
-            } else {
-                dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1])
-            }
-        }
-    }
-    
-    return dp[n][m]
 };
