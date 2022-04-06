c507daaa93586fea259b14cd6e42e8306d09be4d
YunLong Zhou <yunlong.zhou@thoughtworks.com>
2022-04-06 11:49:13 +0800

    algorithm: solution of stone game 👏🏻

--- algorithm/middle/877-stone-game.js
+++ algorithm/middle/877-stone-game.js
@@ -3,10 +3,34 @@
  * @return {boolean}
  */
 
-// f(l, r) => 在[l, r]之间两个玩家的最大差值，差值大于1，先手赢
+// source: https://leetcode-cn.com/problems/stone-game/
+
+// dynamic program
+// 定义dp: dp[i][j] 表示[i, j]区间内两个玩家的差值
+// 状态dp:
+//   f(i, j) = max{ piles[i] - f(i + 1. j), piles[j] - f(i, j - 1) }
+// 初始化: dp[i][j] = 0
+// 边界: i === j & dp[i][j] = piles[i]
+var stoneGame = function(piles) {
+    const n = piles.length
+    
+    const dp = new Array(n).fill(0).map(_ => new Array(n).fill(0))
+    
+    for(let i = 0; i < n; i++) {
+        dp[i][i] = piles[i]
+    }
+    
+    for(let i = 0; i < n; i++) {
+        for(let j = i + 1; j < n; j++) {
+            dp[i][j] = Math.max(piles[i] - dp[i + 1][j], piles[j] - dp[i][j - 1])
+        }
+    }
+    
+    return dp[0][n - 1] > 0
+};
 
 // memory search
-var stoneGame = function(piles) {
+var stoneGame2 = function(piles) {
     function tryGet(piles, l, r) {
         if (l + 1 === r) {
             return piles[l]
@@ -29,26 +53,4 @@ var stoneGame = function(piles) {
     const memo = new Array(piles.length).fill(-1).map(_ => new Array(piles.length).fill(-1))
     
     return tryGet(piles, 0, piles.length - 1)
-};
-
-// dynamic program
-// 定义dp: dp[i][j]表示在[i, j]中两个人之间的差值，所以当i <= j时，区间才有意义
-// 状态转移: f(i, j) = max{ piles[i] - f(i + 1, j), piles[j] - f(i, j - 1) }
-// 边界: i === j & dp[i][j] = piles[i]
-var stoneGame2 = function(piles) {
-    const n = piles.length
-    
-    const dp = new Array(n).fill(0).map(_ => new Array(n).fill(0))
-    
-    for(let i = 0; i < n; i++) {
-        dp[i][i] = piles[i]
-    }
-    
-    for(let i = 0; i < n; i++) {
-        for(let j = i + 1; j < n; j++) {
-            dp[i][j] = Math.max(piles[i] - dp[i + 1][j], piles[j] - dp[i][j - 1])
-        }
-    }
-    
-    return dp[0][n - 1] > 0
 };
