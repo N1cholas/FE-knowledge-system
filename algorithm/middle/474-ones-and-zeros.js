76d44fc8c1eb2b808573cdc9db4f06e01d6fa2f3
YunLong Zhou <yunlong.zhou@thoughtworks.com>
2022-04-06 10:57:14 +0800

    algorithm: solution of ones and zeros 👏🏻

--- algorithm/middle/474-ones-and-zeros.js
+++ algorithm/middle/474-ones-and-zeros.js
@@ -5,8 +5,46 @@
  * @return {number}
  */
 
+// source: https://leetcode-cn.com/problems/ones-and-zeroes/
+
+// dynamic program
+// 定义dp: dp[i][j][k] 表示前i个数，最多有j个0和k个1的最大子集长度
+// 状态转移:
+//   f(i, j, k) = f(i - 1, j, k)
+//   j >= m & k >= n & f(i, j, k) = max{ f(i, j, k), 1 + f(i - 1, j - m, k - n) }
+// 初始化: dp[i][j][k] = 0
+// 边界: dp[0][0][0] = 0
+var findMaxForm = function(strs, m, n) {
+    const zeroAndOne = strs.map(str => [
+        str.split('').filter(s => s === '0').length,
+        str.split('').filter(s => s === '1').length
+    ])
+    
+    const len = strs.length
+    
+    const dp = new Array(len + 1).fill(0).map(
+        _ => new Array(m + 1).fill(0).map(
+            _ => new Array(n + 1).fill(0)
+        )
+    )
+    
+    for(let i = 1; i <= len; i++) {
+        const [zeros, ones] = zeroAndOne[i - 1]
+        for(let j = 0; j <= m; j++) {
+            for(let k = 0; k <= n; k++) {
+                dp[i][j][k] = dp[i - 1][j][k]
+                if (j >= zeros && k >= ones) {
+                    dp[i][j][k] = Math.max(dp[i][j][k], 1 + dp[i - 1][j - zeros][k - ones])
+                }
+            }
+        }
+    }
+    
+    return dp[len][m][n]
+};
+
 // memory search
-var findMaxForm = function (strs, m, n) {
+var findMaxForm2 = function (strs, m, n) {
     const formatStrs = strs.map(str => {
         let ones = 0
         let zeros = 0
@@ -40,41 +78,4 @@ var findMaxForm = function (strs, m, n) {
     const memo = new Map()
     
     return tryFind(formatStrs, formatStrs.length - 1, m, n)
-};
-
-// dynamic program
-// dp定义: dp[i][j][k] 表示前i个strs最多有j个0和个1的最大子集长度
-// 状态转移方程:
-//                      不选择        选择        减去0         减去1
-// f(i, j, k) = max{ f(i, j, k), 1 + f(i - 1, j - strs[i], k - strs[i])}
-// 边界: 当i等0 都有 dp[i][j][k] = 0
-var findMaxForm2 = function(strs, m, n) {
-    const strings = strs.map(
-        str => [
-            str.split('').filter(s => s === '0').length,
-            str.split('').filter(s => s === '1').length
-        ]
-    )
-    
-    const len = strs.length
-    
-    const dp = new Array(len + 1).fill(0).map(
-        _ => new Array(m + 1).fill(0).map(
-            _ => new Array(n + 1).fill(0)
-        )
-    )
-    
-    for (let i = 1; i <= len; i++) {
-        const [zeros, ones] = strings[i - 1]
-        for(let j = 0; j <= m; j++) {
-            for(let k = 0; k <= n; k++) {
-                dp[i][j][k] = dp[i - 1][j][k]
-                if (j >= zeros && k >= ones) {
-                    dp[i][j][k] = Math.max(dp[i][j][k], 1 + dp[i - 1][j - zeros][k - ones])
-                }
-            }
-        }
-    }
-    
-    return dp[len][m][n]
 };
