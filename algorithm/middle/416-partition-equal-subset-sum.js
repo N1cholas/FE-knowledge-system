4bf8909cedcb6634da9aecc756412309bc1b8645
YunLong Zhou <yunlong.zhou@thoughtworks.com>
2022-04-06 10:29:56 +0800

    algorithm: solution of partition equal subset sum 👏🏻

--- algorithm/middle/416-partition-equal-subset-sum.js
+++ algorithm/middle/416-partition-equal-subset-sum.js
@@ -3,8 +3,42 @@
  * @return {boolean}
  */
 
-// memory search
+// source: https://leetcode-cn.com/problems/partition-equal-subset-sum/
+
+// 定义dp: dp[i][j] 代表前i个数的和能否组成j值
+// 状态转移:
+//   j >= nums[i - 1] & f(i， j) = f(i - 1, j) || f(i - 1, j - nums[i - 1])
+//   j < nums[i - 1] & f(i, j) = f(i - 1, j)
+// 初始化: dp[i][j] = false
+// 边界: dp[0][0] = true
 var canPartition = function(nums) {
+    const n = nums.length
+    
+    const sum = nums.reduce((p, c) => p + c)
+    
+    if (sum % 2) return false
+    
+    const target = sum / 2
+    
+    const dp = new Array(n + 1).fill(0).map(_ => new Array(target + 1).fill(false))
+    
+    dp[0][0] = true
+    
+    for (let i = 1; i <= n; i++) {
+        for(let j = 1; j <= target; j++) {
+            if (j >= nums[i - 1]) {
+                dp[i][j] = dp[i - 1][j] || dp[i - 1][j - nums[i - 1]]
+            } else {
+                dp[i][j] = dp[i - 1][j]
+            }
+        }
+    }
+    
+    return dp[n][target]
+};
+
+// memory search
+var canPartition2 = function(nums) {
     function partition(nums, index, sum) {
         if (index < 0 || sum < 0) {
             return false
@@ -30,37 +64,4 @@ var canPartition = function(nums) {
     }
     
     return partition(nums, nums.length - 1, sum / 2)
-};
-
-// dynamic program
-// 定义dp: dp[i][j] 表示前i个nums数中能否找到和为j
-// 状态方程:
-//               不选择nums[i]     选择nums[i]
-//     f(i, j) = f(i - 1, j) || f(i - 1, j - nums[i]) & j >= nums[i]
-//     f(i, j) = f(i - 1, j) & j < nums[i]
-// 边界：f(0, 0) = true
-var canPartition2 = function(nums) {
-    const sum = nums.reduce((p, c) => p + c)
-    
-    if (sum % 2) return false
-    
-    const target = sum / 2
-    
-    const n = nums.length
-    
-    const dp = new Array(n + 1).fill(0).map(_ => new Array(target + 1).fill(false))
-    
-    dp[0][0] = true
-    
-    for(let i = 1; i <= n; i++) {
-        for(let j = 1; j <= target; j++) {
-            if (j >= nums[i - 1]) {
-                dp[i][j] = dp[i - 1][j] || dp[i - 1][j - nums[i - 1]]
-            } else {
-                dp[i][j] = dp[i - 1][j]
-            }
-        }
-    }
-    
-    return dp[n][target]
 };
