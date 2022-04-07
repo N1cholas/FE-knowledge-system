9e1ef50fb9a568e0105be8144917490374b86d9c
YunLong Zhou <yunlong.zhou@thoughtworks.com>
2022-04-07 10:21:34 +0800

    algorithm: solution of number combinations 👏🏻

--- algorithm/middle/017-number-combinations.js
+++ algorithm/middle/017-number-combinations.js
@@ -3,38 +3,41 @@
  * @return {string[]}
  */
 var letterCombinations = function(digits) {
-    const map = new Map([
-        ['1', ''],
-        ['2', 'abc'],
-        ['3', 'def'],
-        ['4', 'ghi'],
-        ['5', 'jkl'],
-        ['6', 'mno'],
-        ['7', 'pqrs'],
-        ['8', 'tuv'],
-        ['9', 'wxyz'],
-        ['0', ' '],
-    ])
-    
-    function tryCombine(digits, index, current, res) {
-        if (index === digits.length) {
-            return res.push(current.slice().join(''))
-        }
-        
-        const alphabet = map.get(digits[index])
-        
-        for (let i = 0; i < alphabet.length; i++) {
-            current.push(alphabet[i])
-            tryCombine(digits, index + 1, current, res)
-            current.pop()
-        }
-    }
+    if (!digits.length) return []
     
     const res = []
-    
-    if (!digits.length) return []
     
     tryCombine(digits, 0, [], res)
     
     return res
 };
+
+const map = new Map([
+    ['1', ''],
+    ['2', 'abc'],
+    ['3', 'def'],
+    ['4', 'ghi'],
+    ['5', 'jkl'],
+    ['6', 'mno'],
+    ['7', 'pqrs'],
+    ['8', 'tuv'],
+    ['9', 'wxyz'],
+    ['0', ' '],
+])
+
+// 定义参数
+// 确定边界
+// 实现回溯
+const tryCombine = (digits, index, current, res) => {
+    if (digits.length === index) {
+        return res.push(current.slice().join(''))
+    }
+    
+    const letters = map.get(digits.at(index))
+    
+    for(let i = 0, len = letters.length; i < len; i++) {
+        current.push(letters.at(i))
+        tryCombine(digits, index + 1, current, res)
+        current.pop()
+    }
+}
