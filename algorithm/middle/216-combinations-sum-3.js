8b848410629d9a2c691f574b5965baaaa930fc95
YunLong Zhou <yunlong.zhou@thoughtworks.com>
2022-04-08 11:40:10 +0800

    algorithm: solution of combinations sum III 👏🏻

--- algorithm/middle/216-combinations-sum-3.js
+++ algorithm/middle/216-combinations-sum-3.js
@@ -3,24 +3,27 @@
  * @param {number} n
  * @return {number[][]}
  */
+
+// source: https://leetcode-cn.com/problems/combination-sum-iii/
+
 var combinationSum3 = function(k, n) {
-    function tryCombine(k, n, index, current, res) {
-        if (n === 0 && k === 0) {
-            return res.push(current.slice())
-        }
-        
-        for (let i = index; i <= 9; i++) {
-            if (n >= i) {
-                current.push(i)
-                tryCombine(k - 1, n - i, i + 1, current, res)
-                current.pop()
-            }
-        }
-    }
-    
     const res = []
     
     tryCombine(k, n, 1, [], res)
     
     return res
 };
+
+const tryCombine = (k, n, index, current, res) => {
+    if (k === 0 && n === 0) {
+        return res.push(current.slice())
+    }
+    
+    for(let i = index; i <= 9; i++) {
+        if (n >= i) {
+            current.push(i)
+            tryCombine(k - 1, n - i, i + 1, current, res)
+            current.pop()
+        }
+    }
+}
