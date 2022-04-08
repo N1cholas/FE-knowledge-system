fbdb545abc28644c5a84cc5c9e3d79b802e38910
YunLong Zhou <yunlong.zhou@thoughtworks.com>
2022-04-08 10:23:29 +0800

    algorithm: solution of restore IP address 👏🏻

--- algorithm/middle/093-restore-IP-address.js
+++ algorithm/middle/093-restore-IP-address.js
@@ -2,39 +2,43 @@
  * @param {string} s
  * @return {string[]}
  */
+
+// source: https://leetcode-cn.com/problems/restore-ip-addresses/
+
 var restoreIpAddresses = function(s) {
-    function tryRestore(s, index, current, res) {
-        if (index >= s.length) {
-            if (current.length === 4) {
-                res.push(current.join('.'))
-            }
-            return
-        }
-        
-        if (index === 0) {
-            current.push(Number(s.charAt(index)))
-            tryRestore(s, index + 1, current, res)
-        } else {
-            const lastIndex = current.length - 1
-            const next = current[lastIndex] * 10 + Number(s.charAt(index))
-            
-            if (next <= 255 && current[lastIndex] !== 0) {
-                current[lastIndex] = next
-                tryRestore(s, index + 1, current, res)
-                current[lastIndex] = ~~(current[lastIndex] / 10)
-            }
-        
-            if (current.length < 4) {
-                current.push(Number(s.charAt(index)))
-                tryRestore(s, index + 1, current, res)
-                current.pop()
-            }
-        }
-    }
-    
     const res = []
     
-    tryRestore(s, 0, [], res)
+    tryRestoreIp(s, 0, [], res)
     
     return res
 };
+
+const tryRestoreIp = (s, index, current, res) => {
+    if (index >= s.length) {
+        if (current.length === 4) {
+            res.push(current.slice().join('.'))
+        }
+        
+        return
+    }
+    
+    if (index === 0) {
+        current.push(Number(s.charAt(index)))
+        tryRestoreIp(s, index + 1, current, res)
+    } else {
+        const last = current[current.length - 1]
+        const next = last * 10 + Number(s.charAt(index))
+        
+        if (next <= 255 && last !== 0) {
+            current[current.length - 1] = next
+            tryRestoreIp(s, index + 1, current, res)
+            current[current.length - 1] = ~~(next / 10)
+        }
+        
+        if (current.length < 4) {
+            current.push(Number(s.charAt(index)))
+            tryRestoreIp(s, index + 1, current, res)
+            current.pop()
+        }
+    }
+}
